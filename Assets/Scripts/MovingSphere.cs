15c5642eced4723d96b31122d5fe0356c283f6b9
Christian Holm <chrholmchr@hotmail.com>
2022-10-17 22:00:40 +0200

    Part 4.1.3 - Focus Radius

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -1,7 +1,19 @@
 /*
  * Original code by Jasper Flick. Catlike Coding. 2019. https://catlikecoding.com/unity/tutorials/movement
  * Original code licensed under the MIT-0 license:      https://catlikecoding.com/unity/tutorials/license/
- * BitBucket repo at:                                   https://bitbucket.org/catlikecodingunitytutorials/movement-01-sliding-a-sphere/src/master/
+ * BitBucket repos at:
+ *     Part 1: Sliding a Sphere      - https://bitbucket.org/catlikecodingunitytutorials/movement-01-sliding-a-sphere/src/master/
+ *     Part 2: Physics               - https://bitbucket.org/catlikecodingunitytutorials/movement-02-physics/src/master/
+ *     Part 3: Surface Contact       - https://bitbucket.org/catlikecodingunitytutorials/movement-03-surface-contact/src/master/
+ *     Part 4: Orbit Camera          - https://bitbucket.org/catlikecodingunitytutorials/movement-04-orbit-camera/src/master/
+ *     Part 5: Custom Gravity        - https://bitbucket.org/catlikecodingunitytutorials/movement-05-custom-gravity/src/master/
+ *     Part 6: Complex Gravity       - https://bitbucket.org/catlikecodingunitytutorials/movement-06-complex-gravity/src/master/
+ *     Part 7: Moving The Ground     - https://bitbucket.org/catlikecodingunitytutorials/movement-07-moving-the-ground/src/master/
+ *     Part 8: Climbing              - https://bitbucket.org/catlikecodingunitytutorials/movement-08-climbing/src/master/
+ *     Part 9: Swimming              - https://bitbucket.org/catlikecodingunitytutorials/movement-09-swimming/src/master/
+ *    Part 10: Reactive Environment  - https://bitbucket.org/catlikecodingunitytutorials/movement-10-reactive-environment/src/master/
+ *    Part 11: Rolling               - https://bitbucket.org/catlikecodingunitytutorials/movement-11-rolling/src/master/
+ * 
  * Adapted and modified by Christian Holm Christensen. October 16th, 2022. 
 */
 
@@ -14,32 +26,43 @@ public class MovingSphere : MonoBehaviour
     [Header("SETTINGS")]
     [Header("> Locomotion")]
     [Header(">> On Ground")]
-    [SerializeField, Range(0f, 90f)] float maxGroundAngle = 25f;
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)] float maxGroundAcceleration = 10f;
+    [CHCReadOnly] public int groundContactCount;
+    [CHCReadOnly] public Vector2 moveInputVector;
+    [CHCReadOnly] public Vector3 desiredVelocity;
+    [CHCReadOnly] public Vector3 velocity;
+    [CHCReadOnly] public Vector3 groundContactNormal;
+    [Header(">>> Movement Angles")]
+    [SerializeField, Range(0f, 90f)] float maxGroundAngle = 25f;
+    [SerializeField, Range(0f, 90f)] float maxStairAngle = 50f;
+    [CHCReadOnly] public int steepContactCount;
+    [CHCReadOnly] public float minGroundDotProduct;
+    [CHCReadOnly] public float minStairDotProduct;
+    [CHCReadOnly] public Vector3 steepContactNormal;
+
+    [Header(">>> Snap To Ground")]
+    [SerializeField, Range(0f, 100f)] float maxSnapToGroundSpeed = 100f;
+    [SerializeField, Min(0f)] float snapToGroundProbeDistance = 1f;
+    [SerializeField] LayerMask groundProbeMask = -1;
+    [SerializeField] LayerMask stairProbeMask = -1;
+    [CHCReadOnly] public int jumpingPhysicsStepsSinceLast;
+    [CHCReadOnly] public int groundedPhysicsStepsSinceLast;
     [Space]
     [Header(">> In Air")]
     [SerializeField, Range(0, 10)] int maxAirJumps;
     [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
     [SerializeField, Range(0f, 100f)] float maxAirAcceleration = 1f;
+    [CHCReadOnly] public int jumpPhase;
+    [CHCReadOnly] public bool hasJumpInput;
     [Space]
     [Header("SCRIPTABLE OBJECTS")]    
     [SerializeField] InputReader inputReader;
-    [Space]
-    [Header("DEBUG")]
-    [CHCReadOnly] public int jumpPhase;
-    [CHCReadOnly] public int groundContactCount;
-    [CHCReadOnly] public bool hasJumpInput;
-    [CHCReadOnly] public float minGroundDotProduct;
-    [CHCReadOnly] public Vector2 moveInputVector;
-    [CHCReadOnly] public Vector3 desiredVelocity;
-    [CHCReadOnly] public Vector3 velocity;
-    [CHCReadOnly] public Vector3 contactNormal;
-        
-    Rigidbody body;
+    [CHCReadOnly] public Rigidbody body;
 
     bool OnGround => groundContactCount > 0;
-
+    bool OnSteep => steepContactCount > 0;
+    bool AirJumpingIsAllowed => maxAirJumps > 0;
     void OnEnable()
     {
         inputReader.MoveInputEvent += vector => moveInputVector = vector;
@@ -50,6 +73,7 @@ public class MovingSphere : MonoBehaviour
     void OnValidate()
     {
         minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+        minStairDotProduct = Mathf.Cos(maxStairAngle * Mathf.Deg2Rad);
     }
 
     void Awake()
@@ -78,43 +102,76 @@ public class MovingSphere : MonoBehaviour
     void ClearState()
     {
         groundContactCount = 0;
-        contactNormal = Vector3.zero;
+        steepContactCount = 0;
+        
+        groundContactNormal = Vector3.zero;
+        steepContactNormal= Vector3.zero;
     }
 
     void UpdateState()
     {
+        groundedPhysicsStepsSinceLast += 1;
+        jumpingPhysicsStepsSinceLast += 1;
+        
         velocity = body.velocity;
         
-        if(OnGround)
+        if(OnGround || SnapToGround() || CheckSteepContacts())
         {
-            jumpPhase = 0;
+            groundedPhysicsStepsSinceLast = 0;
+            CheckForFalseLanding();
+            
             if(groundContactCount > 1)
-                contactNormal.Normalize();
+                groundContactNormal.Normalize();
         }
         else
-            contactNormal = Vector3.up;
+            groundContactNormal = Vector3.up;
     }
-    
+
+
     void HandleJumping()
     {
         if (!hasJumpInput) return;
+        
         hasJumpInput = false;
+        
         Jump();
     }
 
     void Jump()
     {
-        if (!OnGround && jumpPhase >= maxAirJumps) return;
+        Vector3 jumpDirection;
         
+        if (OnGround)
+            jumpDirection = groundContactNormal;
+        else if (OnSteep)
+        {
+            jumpDirection = steepContactNormal;
+            jumpPhase = 0;
+        }
+        else if (AirJumpingIsAllowed && jumpPhase <= maxAirJumps)
+        {
+            PreventExtraJumpAfterSurfaceFall();
+            jumpDirection = groundContactNormal;
+        }
+        else
+            return;
+        
+        jumpingPhysicsStepsSinceLast = 0;
         jumpPhase += 1;
+        
         float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-        float alignedSpeed = Vector3.Dot(velocity, contactNormal);
-
+        
+        jumpDirection = (jumpDirection + Vector3.up).normalized;
+        float alignedSpeed = Vector3.Dot(velocity, jumpDirection);
+        
         if(alignedSpeed > 0f)
             jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
         
-        velocity += contactNormal * jumpSpeed;
+        velocity += jumpDirection * jumpSpeed;
     }
+
+    void CheckForFalseLanding() => jumpPhase = jumpingPhysicsStepsSinceLast > 1 ? 0 : jumpPhase;
+    void PreventExtraJumpAfterSurfaceFall() => jumpPhase = jumpPhase == 0 ? 1 : jumpPhase;
 
     void AdjustVelocity()
     {
@@ -133,7 +190,45 @@ public class MovingSphere : MonoBehaviour
         velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
     }
 
-    Vector3 ProjectOnContactPlane(Vector3 vector) => vector - contactNormal * Vector3.Dot(vector, contactNormal);
+    bool SnapToGround()
+    {
+        if (groundedPhysicsStepsSinceLast > 1 || jumpingPhysicsStepsSinceLast <= 2) return false;
+        
+        float speed = velocity.magnitude;
+        if (speed > maxSnapToGroundSpeed) return false;
+        
+        if (!Physics.Raycast(body.position, Vector3.down, out RaycastHit hit, snapToGroundProbeDistance, groundProbeMask)) return false;
+        if (hit.normal.y < GetMinDotProduct(hit.collider.gameObject.layer)) return false;
+        
+        groundContactCount = 1;
+        groundContactNormal = hit.normal;
+
+        float dot = Vector3.Dot(velocity, hit.normal);
+        if(dot > 0f)
+            velocity = (velocity - hit.normal * dot).normalized * speed;
+        
+        return true;
+    }
+
+    bool CheckSteepContacts()
+    {
+        if (steepContactCount > 1)
+        {
+            steepContactNormal.Normalize();
+            if (steepContactNormal.y >= minGroundDotProduct)
+            {
+                groundContactCount = 1;
+                groundContactNormal = steepContactNormal;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    float GetMinDotProduct(int layer) => (stairProbeMask & (1 << layer)) == 0 ? minGroundDotProduct : minStairDotProduct;
+
+    Vector3 ProjectOnContactPlane(Vector3 vector) => vector - groundContactNormal * Vector3.Dot(vector, groundContactNormal);
     
     void OnJumpInput() => hasJumpInput = true;
     void OnJumpInputCancelled() => hasJumpInput = false;
@@ -142,13 +237,20 @@ public class MovingSphere : MonoBehaviour
     void OnCollisionStay(Collision collision) => EvaluateCollision(collision);
     void EvaluateCollision(Collision collision)
     {
+        float minDotProduct = GetMinDotProduct(collision.gameObject.layer);
         for (int i = 0; i < collision.contactCount; i++)
         {
             Vector3 normal = collision.GetContact(i).normal;
-            if (normal.y < minGroundDotProduct) continue;
-            
-            groundContactCount += 1;
-            contactNormal += normal;
+            if (normal.y >= minDotProduct)
+            {
+                groundContactCount += 1;
+                groundContactNormal += normal;
+            }
+            else if (normal.y > -.01f)
+            {
+                steepContactCount += 1;
+                steepContactNormal += normal;
+            }
         }
     }
 
---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using EditorTools;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class OrbitCamera : MonoBehaviour
+{
+	[Header("SETTINGS")]
+	[SerializeField, Range(1f, 20f)] float cameraDistance = 5f;
+	[Header(">> Focus")]
+	[SerializeField] Transform focusObject;
+	[SerializeField, Min(0f)] float focusRadius = 1f;
+	[CHCReadOnly] public Vector3 focusPoint;
+	
+	Vector3 LocalPosition { get => transform.localPosition; set => transform.localPosition = value; }
+
+	void Awake()
+	{
+		focusPoint = focusObject.position;
+	}
+
+	void LateUpdate()
+	{
+		UpdateFocusPoint();
+		
+		Vector3 lookDirection = transform.forward;
+		LocalPosition = focusPoint - lookDirection * cameraDistance;
+	}
+
+	void UpdateFocusPoint()
+	{
+		Vector3 targetPoint = focusObject.position;
+
+		if (focusRadius > 0f)
+		{
+			float distance = Vector3.Distance(targetPoint, focusPoint);
+
+			if (distance > focusRadius)
+			{
+				focusPoint = Vector3.Lerp(targetPoint, focusPoint, focusRadius / distance);
+			}
+		}
+		else
+			focusPoint = targetPoint;
+	}
+}
